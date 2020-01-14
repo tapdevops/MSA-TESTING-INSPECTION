f69c597a46a5876140bcbb6239f6a6ae980ee7f1
ihsanhusaeri <ihusaeri@gmail.com>
2020-01-14 13:31:24 +0700

    tambah API post

--- app/v1.0/Http/Controllers/InspectionDetailController.js
+++ app/v1.0/Http/Controllers/InspectionDetailController.js
@@ -15,6 +15,62 @@ const InspectionDModel = require(_directory_base + '/app/v1.0/Http/Models/Inspec
  |--------------------------------------------------------------------------
  */
 
+
+/** 
+ 	  * Create
+	  * Untuk menyimpan detail inspeksi yang diinputkan melalui mobile.
+	  * --------------------------------------------------------------------
+	*/
+exports.create = (req, res) => {
+	if (!req.body.INSERT_TIME) {
+		req.body.INSERT_TIME = 'now';
+	}
+	InspectionDModel.findOne({
+		BLOCK_INSPECTION_CODE_D: req.body.BLOCK_INSPECTION_CODE_D
+	})
+		.then(data => {
+			if (data) {
+				res.send({
+					status: true,
+					message: 'Data duplikat',
+					data: []
+				})
+			} else {
+				var auth = req.auth;
+				const set = new InspectionDModel({
+					BLOCK_INSPECTION_CODE_D: req.body.BLOCK_INSPECTION_CODE_D,
+					BLOCK_INSPECTION_CODE: req.body.BLOCK_INSPECTION_CODE,
+					CONTENT_INSPECTION_CODE: req.body.CONTENT_INSPECTION_CODE,
+					VALUE: req.body.VALUE,
+					STATUS_SYNC: req.body.STATUS_SYNC,
+					SYNC_TIME: HelperLib.date_format('now', 'YYYYMMDDhhmmss'),
+					INSERT_USER: req.body.INSERT_USER,
+					INSERT_TIME: HelperLib.date_format(req.body.INSERT_TIME, 'YYYYMMDDhhmmss'),
+					UPDATE_USER: req.body.INSERT_USER,
+					UPDATE_TIME: HelperLib.date_format(req.body.INSERT_TIME, 'YYYYMMDDhhmmss'),
+					DELETE_USER: "",
+					DELETE_TIME: 0
+				});
+				set.save()
+					.then(data => {
+						if (!data) {
+							return res.send({
+								status: false,
+								message: config.app.error_message.create_404,
+								data: {}
+							});
+						}
+					}).catch(err => {
+						res.status(500).send({
+							status: false,
+							message: config.app.error_message.create_500 + ' - 2',
+							data: {}
+						});
+					});
+			}
+		});
+};
+
 /** 
   * Find One
 * Untuk menampilkan 1 row data berdasarkan Block Inspection Detail Code.
